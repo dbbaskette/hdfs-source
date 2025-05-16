970c26912677ef1ade3daead4fb3727b7873321f
Dan Baskette <dbbaskette@gmail.com>
2025-05-16 16:47:32 -0400

    Fix ApplicationRunner import to use org.springframework.boot.ApplicationRunner

--- src/main/java/com/baskettecase/hdfssource/HdfsSourceApplication.java
+++ src/main/java/com/baskettecase/hdfssource/HdfsSourceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
